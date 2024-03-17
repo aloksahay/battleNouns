6ff1c00cca6a98716a887f7db08821c216eef7b0
rere <regis-graptin@sfr.fr>
2024-03-17 02:16:13 +0100

    update port

--- front/server.js
+++ front/server.js
@@ -6,7 +6,7 @@ const Web3 = require('web3');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // import ContractFactory from 'ethers';
 // import { ethers } from 'ethers';
